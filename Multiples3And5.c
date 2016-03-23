4ca080ed8462f6fcc6414a2a25eaf5a7a8bec4c6
Adilijiang <anharadiljan@gmail.com>
2016-03-22 23:10:12 -0700

    Problem 1 finished

---
+++ Multiples3And5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int sum = 0;
+  int i = 0;
+  while (i < 1000){
+    if (i % 3 == 0 || i % 5 == 0)
+      sum += i;
+    i++;
+  }
+  printf("sum = %d\n", sum);
+  return 0;
+}
